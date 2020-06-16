f642fa6f201283e2c0989c21ae597e0bfa04ce97
charles <596147986@qq.com>
2020-06-16 15:32:32 +1000

    upload 2

--- gen_lib.sh
+++ gen_lib.sh
@@ -6,17 +6,17 @@ cd SVF
 
 if [[ $sysOS == "Darwin" ]]
 then
- tar -cvf /home/travis/build/charles32110/autogen_svf_lib/svflib_mac.tar.gz lib/libsvf_mac.a lib/CUDD/libCudd.a
-  cd /home/travis/build/charles32110/autogen_svf_lib
+ tar -cvf /home/travis/build/charles32110/autogen_svf_lib/autogen_svf_lib/svflib_mac.tar.gz lib/libsvf_mac.a lib/CUDD/libCudd.a
+  cd /home/travis/build/charles32110/autogen_svf_lib/autogen_svf_lib
   git add svflib_mac.tar.gz
   git commit -m "new_version"
-  git push
+  git push origin
 elif [[ $sysOS == "Linux" ]]
 then
-  tar -cvf /home/travis/build/charles32110/autogen_svf_lib/svflib_ubuntu.tar.gz lib/libsvf_ubuntu.a lib/CUDD/libCudd.a
-  cd /home/travis/build/charles32110/autogen_svf_lib
+  tar -cvf /home/travis/build/charles32110/autogen_svf_lib/autogen_svf_lib/svflib_ubuntu.tar.gz lib/libsvf_ubuntu.a lib/CUDD/libCudd.a
+  cd /home/travis/build/charles32110/autogen_svf_lib/autogen_svf_lib
   git add svflib_ubuntu.tar.gz
   git commit -m "new_version"
-  git push
+  git push origin
 fi
 
