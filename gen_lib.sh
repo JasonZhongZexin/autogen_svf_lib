16d54026369d8ddf6cce22bd90cabc37fd77cc0b
charles <596147986@qq.com>
2020-06-24 15:11:19 +1000

    as

--- gen_lib.sh
+++ gen_lib.sh
@@ -6,6 +6,6 @@ then
  tar -cvf /home/travis/build/charles32110/autogen_svf_lib/svflib_mac.tar.gz /home/travis/build/charles32110/autogen_svf_lib/SVF/Release-build/lib/libsvf_mac.a /home/travis/build/charles32110/autogen_svf_lib/SVF/Release-build/lib/CUDD/libCudd.a
 elif [[ $sysOS == "Linux" ]]
 then
-  tar -cvf /home/travis/build/charles32110/autogen_svf_lib/svflib_ubuntu.tar.gz lib/libsvf_ubuntu.a lib/CUDD/libCudd.a
+  tar -cvf /home/travis/build/charles32110/autogen_svf_lib/svflib_ubuntu.tar.gz /home/travis/build/charles32110/autogen_svf_lib/SVF/Release-build/lib/libsvf_ubuntu.a /home/travis/build/charles32110/autogen_svf_lib/SVF/Release-build/lib/CUDD/libCudd.a
 fi
 
