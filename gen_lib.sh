c7698daa624439f1e4b1eb1a2613ab6ced8f8e56
charles <596147986@qq.com>
2020-06-24 12:23:59 +1000

    new test1

--- gen_lib.sh
+++ gen_lib.sh
@@ -3,7 +3,7 @@ sysOS=`uname -s`
 
 if [[ $sysOS == "Darwin" ]]
 then
- tar -cvf /home/travis/build/charles32110/autogen_svf_lib/autogen_svf_lib/svflib_mac.tar.gz lib/libsvf_mac.a lib/CUDD/libCudd.a
+ tar -cvf /home/travis/build/charles32110/autogen_svf_lib/autogen_svf_lib/svflib_mac.tar.gz /home/travis/build/charles32110/autogen_svf_lib/SVF/Release-build/lib/libsvf_mac.a lib/CUDD/libCudd.a
 elif [[ $sysOS == "Linux" ]]
 then
   tar -cvf /home/travis/build/charles32110/autogen_svf_lib/autogen_svf_lib/svflib_ubuntu.tar.gz lib/libsvf_ubuntu.a lib/CUDD/libCudd.a
