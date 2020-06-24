565699864eb4e7cc4769380511934a3bfa5b0b1c
charles <596147986@qq.com>
2020-06-24 15:41:09 +1000

    as

--- gen_lib.sh
+++ gen_lib.sh
@@ -1,11 +1,11 @@
 # bash!
 sysOS=`uname -s`
-
+PWD
 if [[ $sysOS == "Darwin" ]]
 then
- tar -cvf $PWD/svflib_mac.tar.gz $PWD/SVF/Release-build/lib/libsvf_mac.a $PWD/SVF/Release-build/lib/CUDD/libCudd.a
+ tar -cvf $PWD/svflib_mac.tar.gz $PWD/SVF/Release-build/lib/libSvf.a $PWD/SVF/Release-build/lib/CUDD/libCudd.a
 elif [[ $sysOS == "Linux" ]]
 then
-  tar -cvf $PWD/svflib_ubuntu.tar.gz $PWD/SVF/Release-build/lib/libsvf_ubuntu.a $PWD/SVF/Release-build/lib/CUDD/libCudd.a
+  tar -cvf $PWD/svflib_ubuntu.tar.gz $PWD/SVF/Release-build/lib/libSvf.a $PWD/SVF/Release-build/lib/CUDD/libCudd.a
 fi
 
