bf3d1f0dec02b485ab9c44ee78a25f73661b4023
JasonZhongZexin <samsung@svf-tester.asia-east2-a.c.v2ray-project-267121.internal>
2020-06-25 03:56:18 +0000

    update tar path

--- gen_lib.sh
+++ gen_lib.sh
@@ -3,9 +3,9 @@ sysOS=`uname -s`
 pwd
 if [[ $sysOS == "Darwin" ]]
 then
- tar -cvf $PWD/svflib.tar.gz $PWD/SVF/Release-build/lib/libSvf.a $PWD/SVF/Release-build/lib/CUDD/libCudd.a
+ tar -cvf $PWD/svflib.tar.gz -C $PWD/SVF/Release-build/lib/libSvf.a -C $PWD/SVF/Release-build/lib/CUDD/libCudd.a
 elif [[ $sysOS == "Linux" ]]
 then
-  tar -cvf $PWD/svflib.tar.gz $PWD/SVF/Release-build/lib/libSvf.a $PWD/SVF/Release-build/lib/CUDD/libCudd.a
+  tar -cvf $PWD/svflib.tar.gz -C $PWD/SVF/Release-build/lib/libSvf.a -C $PWD/SVF/Release-build/lib/CUDD/libCudd.a
 fi
 ls
