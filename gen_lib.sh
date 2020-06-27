3075fccd4a52eaee6131930da5482eef2973daa2
JasonZhongZexin <samsung@svf-tester.asia-east2-a.c.v2ray-project-267121.internal>
2020-06-27 14:42:02 +0000

    update

--- gen_lib.sh
+++ gen_lib.sh
@@ -4,13 +4,11 @@ pwd
 home=$pwd
 echo $home
 git clone https://github.com/SVF-tools/SVF-npm.git
+cp -rf ./SVF/include ./SVF-npm/
 if [[ $sysOS == "Darwin" ]]
 then
-  cp ./SVF/Release-build/lib/libSvf.a libSvf_mac.a
-  cp ./SVF/Release-build/lib/CUDD/libCudd.a libCudd_mac.a
-  cp ./libSvf_mac.a ./SVF-npm/Release-build/lib
-  cp ./libCudd_mac.a ./SVF-npm/Release-build/lib
-  cp -rf ./SVF/include ./SVF-npm/
+  cp ./SVF/Release-build/lib/libSvf.a ./SVF-npm/mac/Release-build/lib
+  cp ./SVF/Release-build/lib/CUDD/libCudd.a ./SVF-npm/mac/Release-build/lib
   cd SVF-npm
   git status
   git add .
@@ -18,11 +16,8 @@ then
   git status
 elif [[ $sysOS == "Linux" ]]
 then
-  cp ./SVF/Release-build/lib/libSvf.a libSvf_ubuntu.a
-  cp ./SVF/Release-build/lib/CUDD/libCudd.a libCudd_ubuntu.a
-  cp ./libSvf_ubuntu.a ./SVF-npm/Release-build/lib
-  cp ./libCudd_ubuntu.a ./SVF-npm/Release-build/lib
-  cp -rf ./SVF/include ./SVF-npm/
+  cp ./SVF/Release-build/lib/libSvf.a ./SVF-npm/ubuntu/Release-build/lib
+  cp ./SVF/Release-build/lib/CUDD/libCudd.a ./SVF-npm/ubuntu/Release-build/lib
   cd SVF-npm
   git status
   git add .
