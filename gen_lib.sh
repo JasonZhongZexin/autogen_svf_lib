52483ed759ac6ef6692eaef4037fac773723996a
JasonZhongZexin <samsung@svf-tester.asia-east2-a.c.v2ray-project-267121.internal>
2020-06-27 16:06:03 +0000

    update

--- gen_lib.sh
+++ gen_lib.sh
@@ -6,12 +6,12 @@ if [[ $sysOS == "Darwin" ]]
 then
   cp -rf ./SVF/mac/include ./SVF-npm/
   cp ./SVF/Release-build/lib/libSvf.a ./SVF-npm/mac/Release-build/lib
-  cp ./SVF/Release-build/lib/CUDD/libCudd.a ./SVF-npm/mac/Release-build/lib
+  cp ./SVF/Release-build/lib/CUDD/libCudd.a ./SVF-npm/mac/Release-build/lib/CUDD
 elif [[ $sysOS == "Linux" ]]
 then
   cp -rf ./SVF/ubuntu/include ./SVF-npm/
   cp ./SVF/Release-build/lib/libSvf.a ./SVF-npm/ubuntu/Release-build/lib
-  cp ./SVF/Release-build/lib/CUDD/libCudd.a ./SVF-npm/ubuntu/Release-build/lib
+  cp ./SVF/Release-build/lib/CUDD/libCudd.a ./SVF-npm/ubuntu/Release-build/lib/CUDD
 fi
 cd SVF-npm
 git add .
