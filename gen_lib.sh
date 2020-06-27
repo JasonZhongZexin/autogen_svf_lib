c3ec04b6bfb4ad228b1a4bd0d4ea44d039edcfa0
JasonZhongZexin <samsung@svf-tester.asia-east2-a.c.v2ray-project-267121.internal>
2020-06-27 15:32:37 +0000

    update

--- gen_lib.sh
+++ gen_lib.sh
@@ -1,27 +1,19 @@
 # bash!
 sysOS=`uname -s`
-pwd
 home=$pwd
-echo $home
 git clone https://github.com/SVF-tools/SVF-npm.git
-cp -rf ./SVF/include ./SVF-npm/
 if [[ $sysOS == "Darwin" ]]
 then
+  cp -rf ./SVF/mac/include ./SVF-npm/
   cp ./SVF/Release-build/lib/libSvf.a ./SVF-npm/mac/Release-build/lib
   cp ./SVF/Release-build/lib/CUDD/libCudd.a ./SVF-npm/mac/Release-build/lib
-  cd SVF-npm
-  git status
-  git add .
-  git commit -m'update svflib'
-  git status
 elif [[ $sysOS == "Linux" ]]
 then
+  cp -rf ./SVF/ubuntu/include ./SVF-npm/
   cp ./SVF/Release-build/lib/libSvf.a ./SVF-npm/ubuntu/Release-build/lib
   cp ./SVF/Release-build/lib/CUDD/libCudd.a ./SVF-npm/ubuntu/Release-build/lib
-  cd SVF-npm
-  git status
-  git add .
-  git commit -m'update svflib'
-  git status
 fi
+cd SVF-npm
+git add .
+git commit -m'update svflib'
 ls
